b9d2e862873c89863af6fafed7fdcd3e9d423fc8
Matheus Lira <matheus@avanzzada.com.br>
2022-11-22 18:32:08 -0300

    chore: add getRepositoriesByQuery graphql query

---
+++ src/graphql/queries/getRepositoriesByQuery.ts
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const GET_REPOSITORIES_BY_QUERY = gql`
+  query GetRepositoriesByQuery($searchTerm: String!) {
+    search(query: $searchTerm, type: REPOSITORY, first: 100) {
+      edges {
+        node {
+          ... on Repository {
+            id
+            name
+            owner {
+              login
+            }
+            url
+            forkCount
+            stargazerCount
+          }
+        }
+      }
+    }
+  }
+`;
